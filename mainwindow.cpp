e6d171368d47d8da325668aecce5aa65edd8ed2f
Dima Vladimirov <mosb-191_724852@volsu.ru>
2021-02-14 20:28:05 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,23 +4,29 @@
 
 Widget::Widget(QWidget *parent) : QWidget(parent) {
 
+    //рейсаз окна виджета
     this->resize(1150,370);
-
+    
+    //создаю площадку для таблицы, предопределяя 10x10 ячеек
     matrix = new QTableWidget(10,10,this);
-
-
-    matrixF();
-
+    
+    //выставляю размер и положение
     matrix->setGeometry(0,0, 1100, 370);
-
+    
+    //создаю ячейки и заполняю случ числами
+    matrixF();
+    
+    //кнопочка для смены чисел
     act = new QPushButton("Сменить числа");
-
+    
+    //выводимые данные о ячейке
     pos_c = new QLabel;
     pos_r = new QLabel;
     number = new QLabel;
     la_2 = new QLabel;
     la_3 = new QLabel;
-
+    
+    //структурирую в лейаут окна выводимой информации
     QVBoxLayout* infoLabels = new QVBoxLayout;
     infoLabels->addWidget(act);
     infoLabels->addWidget(pos_c);
@@ -29,41 +35,65 @@ Widget::Widget(QWidget *parent) : QWidget(parent) {
     infoLabels->addWidget(la_2);
     infoLabels->addWidget(la_3);
 
+    //собираю таблицу и инфолейблы в структуру
     QHBoxLayout* layout = new QHBoxLayout;
     layout->addWidget(matrix);
     layout->addLayout(infoLabels);
-
-    connect(act,SIGNAL(clicked()), this,SLOT(matrixF()));
-   // connect(matrix, SIGNAL(cellClicked(int, int)), SLOT(itemClicked(int, int)));
+    
+    //устанавливаю стили
     setLayout(layout);
+    
+    //Соединяю кнопку смены чисел и пересоздание ячеек
+    connect(act,SIGNAL(clicked()), this,SLOT(matrixF()));
+    //Соединяю клик по ячейки и вывод информации о ячейке
+    connect(matrix, SIGNAL(cellClicked(int, int)), SLOT(itemClicked(int, int)));
+    
 }
 
 void Widget::matrixF() {
+    
+    //пробегаемся по всем координатам таблицы
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
+            //создаю ячейку
             QTableWidgetItem *item = new QTableWidgetItem();
+            
+            //ставлю флаг на запрет редактированию
             item->setFlags(item->flags() & ~(Qt::ItemIsEditable));
+            
+            //случайное значения в ячейку
             int n = rand()% 20;
             QString s = QString::number(n);
             item->setText(s);
+            
+            //ТулТип для вывода значения по наведению мыши
             item->setToolTip(s);
+            
+            //закидываю в матрицу ячейку по адрессу i, j
             matrix->setItem(i,j,item);
         }
     }
+    
 }
 
 
-void Widget::itemClicked(int column, int row)
-{
+void Widget::itemClicked(int column, int row) {
+    
+    //ячейка для обработки
     QTableWidgetItem *item = new QTableWidgetItem;
+    
+    //получаю в неё данные нажатой ячейки
     item = matrix->item(column,row);
 
+    //вычисляю порядковый номер числа
     int num = column * 10 + row + 1;
-
+    
+    //вывожу очевидное
     pos_c->setText("Строка: " + QString::number(column + 1));
     pos_r->setText("Колонка: " + QString::number(row + 1));
     number->setText("Номер: " + QString::number(num));
 
+    //Далее считаю квадрат и куб и тоже вывожу
     QString data = item->text();
 
     int x = data.toInt();
@@ -81,12 +111,11 @@ void Widget::itemClicked(int column, int row)
 }
 
 void Widget::keyPressEvent(QKeyEvent *event) {
+    
+    //горячая клавиша для заполнения новыми числами
     if (event->key() == Qt::Key_Shift) {
         matrixF();
     }
-    while (event->key() == Qt::Key_Control && matrix->isActiveWindow()) {
-        connect(matrix, SIGNAL(cellClicked(int, int)), SLOT(itemClicked(int, int)));
-    }
-
+    
 }
 
