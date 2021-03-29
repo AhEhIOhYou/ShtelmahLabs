be8b604562490cbc9adb29b18bb6a73c8fc72103
Dima Vladimirov <mosb-191_724852@volsu.ru>
2021-03-29 18:36:41 +0300

    Update 5.cs

--- 5.cs
+++ 5.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
 /*
- * Описатькласс "книга", содержащий следующие закрытые поля: автор, год издания, жанр.
+ * Описать класс "книга", содержащий следующие закрытые поля: автор, год издания, жанр.
  * Предусмотреть свойства для получения состояния объекта. Описать класс "домашняя библиотека".
  * Предусмотреть возможность 
     • работы с произвольным числом книг, 
@@ -22,7 +22,43 @@ namespace _5
 
             Библиотека ДомашняяБиблиотека = new Библиотека();
 
-            ДомашняяБиблиотека.Добавить();
+            ДомашняяБиблиотека.Добавить("k", "Автор-1", "2020", "Жанр-1");
+            char d;
+            bool f = true;
+            while(f)
+            {
+                Console.WriteLine("\n0 - Выйти" +
+                    "\n1 - Добавить" +
+                    "\n2 - Информация о книге" +
+                    "\n3 - Список всех книг" +
+                    "\n4 - Удалить книгу" +
+                    "\n5 - Найти по названию");
+
+                Console.Write("Действие: ");
+                d = Char.Parse(Console.ReadLine());
+                switch(d)
+                {
+                    case '0':
+                        f = false;
+                        break;
+                    case '1':
+                        ДомашняяБиблиотека.Добавить();
+                        break;
+                    case '2':
+                        ДомашняяБиблиотека.Инфо();
+                        break;
+                    case '3':
+                        ДомашняяБиблиотека.ВесьСписок();
+                        break;
+                    case '4':
+                        ДомашняяБиблиотека.Удалить();
+                        break;
+                    case '5':
+                        ДомашняяБиблиотека.Найти();
+                        break;
+                }
+            }
+
         }
     }
 
@@ -42,19 +78,23 @@ namespace _5
             Жанр = жанр;
         }
 
-        public void оКниге()
+        public override string ToString()
         {
-            Console.WriteLine($"Название: {Название}\nАвтор: {Автор}\nГод: {Год}\nЖанр: {Жанр}\n");
+            string str = $"Название: {Название}\nАвтор: {Автор}\nГод: {Год}\nЖанр: {Жанр}\n";
+
+            if (String.IsNullOrEmpty(str))
+                return base.ToString();
+            return str;
         }
     }
 
     class Библиотека
     {
-        private List<Книга> Книги;
+        private List<Книга> СписокКниг;
 
         public Библиотека()
         {
-            Книги = new List<Книга>();
+            СписокКниг = new List<Книга>();
         }
 
         public void Добавить()
@@ -73,18 +113,18 @@ namespace _5
                 Console.Write("Жанр: ");
                 string жанр = Console.ReadLine();
 
-                Книги.Add(new Книга(название, автор, год, жанр));
+                СписокКниг.Add(new Книга(название, автор, год, жанр));
             }
         }
 
         public void Добавить(string название, string автор, string год, string жанр)
         {
-            Книги.Add(new Книга(название, автор, год, жанр));
+            СписокКниг.Add(new Книга(название, автор, год, жанр));
         }
 
         public IEnumerator GetEnumerator()
         {
-            return Книги.GetEnumerator();
+            return СписокКниг.GetEnumerator();
         }
 
         public void ВесьСписок()
@@ -92,10 +132,10 @@ namespace _5
             Console.WriteLine("\nСписок всех книг:");
             int i = 0;
 
-            foreach (Книга книга in Книги)
+            foreach (Книга книга in СписокКниг)
             {
                 Console.WriteLine($"Номер: {i}");
-                книга.оКниге();
+                Console.WriteLine(книга.ToString());
                 i++;
             }
         }
@@ -105,39 +145,76 @@ namespace _5
             Console.Write("По книге номер: ");
             int index = Int32.Parse(Console.ReadLine());
             Console.WriteLine($"Номер: {index}");
-            Книги[index].оКниге();
+            Console.WriteLine(СписокКниг[index].ToString());
         }
 
         public void Инфо(int index)
         {
             Console.WriteLine($"Номер: {index}");
-            Книги[index].оКниге();
+            Console.WriteLine(СписокКниг[index].ToString());
         }
 
         public void Удалить()
         {
             Console.Write("Под каким номером удалить?: ");
             int deleteIndex = Int32.Parse(Console.ReadLine());
-            Console.WriteLine($"\nКнига под номером - {deleteIndex} с названием: {Книги[deleteIndex].Название} успешно удалена");
-            Книги.RemoveAt(deleteIndex);
+            Console.WriteLine($"\nКнига под номером - {deleteIndex} с названием: {СписокКниг[deleteIndex].Название} успешно удалена");
+            СписокКниг.RemoveAt(deleteIndex);
         }
 
         public void Удалить(int deleteIndex)
         {
-            Console.WriteLine($"\nКнига под номером - {deleteIndex} с названием: {Книги[deleteIndex].Название} успешно удалена");
-            Книги.RemoveAt(deleteIndex);
+            Console.WriteLine($"\nКнига под номером - {deleteIndex} с названием: {СписокКниг[deleteIndex].Название} успешно удалена");
+            СписокКниг.RemoveAt(deleteIndex);
         }
 
-        public string Найти(string Name)
+        public Книга Найти()
         {
-            Книга check = Книги.Find(x => x.Название.Contains(Name));
-            if (check != null)
-            {
-                check.оКниге();
-                return "Нашлось!";
-            }
+            Console.Write("Название: ");
+            string name = Console.ReadLine();
 
-            return "Не нашлось :(";
+            foreach (Книга книга in СписокКниг)
+            {
+                if (книга.Название == name)
+                {
+                    Console.WriteLine("Найдено");
+                    Console.WriteLine(книга.ToString());
+                    return книга;
+                }
+            }
+            Console.WriteLine("Не Найдено");
+            return null;
+        }
+
+        public Книга Найти(string Name)
+        {
+            foreach(Книга книга in СписокКниг)
+            {
+                if(книга.Название == Name)
+                {
+                    Console.WriteLine("Найдено");
+                    return книга;
+                }
+            }
+            Console.WriteLine("Не Найдено");
+            return null;
+            
+        }
+
+        public void НайтиВсе()
+        {
+            Console.Write("Название: ");
+            string name = Console.ReadLine();
+
+            foreach (Книга книга in СписокКниг)
+            {
+                if (книга.Название == name)
+                {
+                    Console.WriteLine("Найдено");
+                    Console.WriteLine(книга.ToString());
+                }
+            }
+            Console.WriteLine("Не Найдено");
         }
 
     }
